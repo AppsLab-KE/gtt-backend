cfee954e06ef96567eaf6c3c01af294d098f6953
Brian Gikonyo <colasgikonyo@gmail.com>
2019-07-31 17:17:47 +0300

    few changes to social auth

--- gtt/gtt/settings.py
+++ gtt/gtt/settings.py
@@ -119,6 +119,21 @@ AUTHENTICATION_BACKENDS = (
     'guardian.backends.ObjectPermissionBackend',
 )
 
+SOCIAL_AUTH_PIPELINE = (
+    'users.pipeline.auto_logout',
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'users.pipeline.check_for_email',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    'users.pipeline.save_avatar',
+)
+
 # Github configuration
 SOCIAL_AUTH_GITHUB_KEY = os.getenv('SOCIAL_AUTH_GITHUB_KEY')
 SOCIAL_AUTH_GITHUB_SECRET = os.getenv('SOCIAL_AUTH_GITHUB_SECRET')
